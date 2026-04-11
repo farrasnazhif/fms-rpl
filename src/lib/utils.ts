4db930dea0d6f57617f987af7dc5e1c3fcfb7809
farrasnazhif <farrasnazhif@gmail.com>
2026-04-11 17:32:20 +0700

    feat: resolved image

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function resolveImageUrl(image: string): string {
-  if (image.startsWith("http://") || image.startsWith("https://")) {
-    return image;
-  }
-  const baseURL = process.env.NEXT_PUBLIC_API_URL ?? "";
-  const apiRoot = baseURL.replace(/\/api\/v\d+\/?$/, "");
-  return `${apiRoot}/api/static/${image}`;
+export function resolveImageUrl(path?: string) {
+  if (!path) return "";
+
+  return `https://film-management-api.labse.id/api/static/${path}`;
 }
