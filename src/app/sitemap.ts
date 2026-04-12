9f882e8ab139a380561d483745dc091aa05953e2
farrasnazhif <farrasnazhif@gmail.com>
2026-04-12 07:27:35 +0700

    fix: robots & sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ["/", "/sandbox", "/sandbox/button", "/sandbox/input", "/sandbox/card"];
+  const routes = ["/", "/genres"];
 
   return routes.map((route) => ({
     url: `${siteUrl}${route}`,
